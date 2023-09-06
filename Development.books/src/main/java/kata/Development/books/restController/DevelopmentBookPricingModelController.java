e4bd96fecfb128ae3f52c91a29c85e9bc48470f0
digiovpa <paolodigiov@gmail.com>
2023-09-06 21:32:22 +0200

    - Used a map instead of a list to report the quantity of each book. - Implemented a solution to make the unit test of the example works - Some minor refactoring

--- Development.books/src/main/java/kata/Development/books/restController/DevelopmentBookPricingModelController.java
+++ Development.books/src/main/java/kata/Development/books/restController/DevelopmentBookPricingModelController.java
@@ -5,15 +5,15 @@ import kata.Development.books.entity.DevelopmentBook;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
-import java.util.List;
+import java.util.*;
 
 public class DevelopmentBookPricingModelController {
 
-    public BigDecimal computeDevelopmentBookBasketPrice(List<DevelopmentBook> developmentBookBasket) {
+    public BigDecimal computeDevelopmentBookBasketPrice(Map<DevelopmentBook, Integer> developmentBookBasketMap) {
         BigDecimal finalPrice = new BigDecimal(BigInteger.ZERO);
         BigDecimal developmentBookPrice = new BigDecimal(50);
-        List<DevelopmentBook> distinctBooks = developmentBookBasket.stream().distinct().toList();
-        if (distinctBooks.size() == developmentBookBasket.size()) {
+        //List<DevelopmentBook> distinctBooks = developmentBookBasket.stream().distinct().toList();
+      /*  if (distinctBooks.size() == developmentBookBasket.size()) {
             switch (developmentBookBasket.size()) {
                 case 1 -> {
                     return developmentBookPrice;
@@ -31,12 +31,50 @@ public class DevelopmentBookPricingModelController {
                     return developmentBookPrice.multiply(new BigDecimal(5)).subtract(percentage(developmentBookPrice.multiply(new BigDecimal(5)), new BigDecimal(25)));
                 }
             }
-        }
+        } else {*/
+            List<Integer> developmentBooksQuantity = new ArrayList<>(developmentBookBasketMap.values().stream().sorted(Comparator.reverseOrder()).toList());
+            List<Integer> tempDevelopmentBooksQuantity = new ArrayList<>();
+
+            for (int i = 1; i <= developmentBooksQuantity.get(0); i++) {
+                ListIterator<Integer> iterator = developmentBooksQuantity.listIterator();
+                while (iterator.hasNext()) {
+                    Integer currentValue = iterator.next();
+                    if (currentValue > 1 && currentValue - i >= 0) {
+                        tempDevelopmentBooksQuantity.add(currentValue);
+                    } else if (currentValue <= 1) {
+                        iterator.remove();
+                        tempDevelopmentBooksQuantity.add(0);
+                        break;
+                    } else {
+                        iterator.remove();
+                        break;
+                    }
+                }
+                switch (tempDevelopmentBooksQuantity.size()) {
+                    case 1 -> {
+                        finalPrice = finalPrice.add(developmentBookPrice);
+                    }
+                    case 2 -> {
+                        finalPrice = finalPrice.add(developmentBookPrice.multiply(new BigDecimal(4)).subtract(percentage(developmentBookPrice.multiply(new BigDecimal(2)), new BigDecimal(5))));
+                    }
+                    case 3 -> {
+                        finalPrice = finalPrice.add(developmentBookPrice.multiply(new BigDecimal(4)).subtract(percentage(developmentBookPrice.multiply(new BigDecimal(3)), new BigDecimal(10))));
+                    }
+                    case 4 -> {
+                        finalPrice = finalPrice.add(developmentBookPrice.multiply(new BigDecimal(4)).subtract(percentage(developmentBookPrice.multiply(new BigDecimal(4)), new BigDecimal(20))));
+                    }
+                    case 5 -> {
+                        finalPrice = finalPrice.add(developmentBookPrice.multiply(new BigDecimal(5)).subtract(percentage(developmentBookPrice.multiply(new BigDecimal(5)), new BigDecimal(25))));
+                    }
+                }
+                tempDevelopmentBooksQuantity.clear();
+            }
+
         return finalPrice;
     }
 
 
-    public BigDecimal percentage(BigDecimal base, BigDecimal percent){
+    private BigDecimal percentage(final BigDecimal base, final BigDecimal percent){
         return base.multiply(percent).divide(new BigDecimal(100), 2, RoundingMode.CEILING);
     }
 }
--- Development.books/src/test/java/kata/Development/books/ApplicationTests.java
+++ Development.books/src/test/java/kata/Development/books/ApplicationTests.java
@@ -10,7 +10,9 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,6 +31,8 @@ class ApplicationTests {
 
 	private DevelopmentBookPricingModelController developmentBookPricingModelController;
 
+	private Map<DevelopmentBook, Integer> developmentBookBasketMap;
+
 	@BeforeEach
 	void setup () {
 		cleanCode = new DevelopmentBook(1L, "Clean Code", "Robert Martin");
@@ -37,50 +41,26 @@ class ApplicationTests {
 		testDrivenDevelopmentByExample = new DevelopmentBook(4L, "Test Driven Development by Example", "Kent Beck");
 		workingEffectivelyWithLegacyCode = new DevelopmentBook(5L, "Working effectively with Legacy Code", "Michael C. Feathers");
 		developmentBookPricingModelController = new DevelopmentBookPricingModelController();
+		developmentBookBasketMap = new HashMap<>();
 	}
 	@Test
 	void developmentBooksWithDuplicatesComputePriceTest() {
-     /*
 
-    	2 different books -> 5% discount on those 2 books.
-		3 different books -> 10% discount on those 3 books.
-		4 different books -> 20% discount on those 4 books.
-		All 5 -> 25% discount on those 5 books.
+		developmentBookBasketMap.put(cleanCode, 2);
+		developmentBookBasketMap.put(cleanCoder, 2);
+		developmentBookBasketMap.put(cleanArchitecture, 2);
+		developmentBookBasketMap.put(testDrivenDevelopmentByExample, 1);
+		developmentBookBasketMap.put(workingEffectivelyWithLegacyCode, 1);
 
-		2 copies of the “Clean Code” book
-		2 copies of the “Clean Coder” book
-		2 copies of the “Clean Architecture” book
-		1 copy of the “Test Driven Development by Example” book
-		1 copy of the “Working effectively with Legacy Code” book
+		BigDecimal expectedPrice = new BigDecimal("320").setScale(2);
 
-		Answer :
+		BigDecimal finalPrice = developmentBookPricingModelController.computeDevelopmentBookBasketPrice(developmentBookBasketMap);
 
-		(4 * 50 EUR) - 20% [first book, second book, third book, fourth book]
-		(4 * 50 EUR) - 20% [first book, second book, third book, fifth book]
-        = 160 EUR + 160 EUR = 320 EUR
-
-      */
-		List<DevelopmentBook> developmentBookBasket = new ArrayList<>();
-
-		developmentBookBasket.add(cleanCode);
-		developmentBookBasket.add(cleanCode);
-		developmentBookBasket.add(cleanCoder);
-		developmentBookBasket.add(cleanCoder);
-		developmentBookBasket.add(cleanArchitecture);
-		developmentBookBasket.add(cleanArchitecture);
-		developmentBookBasket.add(testDrivenDevelopmentByExample);
-		developmentBookBasket.add(workingEffectivelyWithLegacyCode);
-
-		BigDecimal expectedPrice = new BigDecimal("320");
-
-		BigDecimal finalPrice = developmentBookPricingModelController.computeDevelopmentBookBasketPrice(developmentBookBasket);
-
-		assertEquals(finalPrice, expectedPrice);
+		assertEquals(expectedPrice, finalPrice);
 	}
 	@Test
 	public void developmentBooksWithoutDuplicatesComputePriceTest() {
-		List<DevelopmentBook> developmentBookBasket = new ArrayList<>();
-
+		/*List<DevelopmentBook> developmentBookBasket = new ArrayList<>();
 		developmentBookBasket.add(cleanCode);
 		developmentBookBasket.add(cleanCoder);
 		developmentBookBasket.add(cleanArchitecture);
@@ -90,7 +70,7 @@ class ApplicationTests {
 		BigDecimal expectedPrice = new BigDecimal("187.50");
 
 		BigDecimal finalPrice = developmentBookPricingModelController.computeDevelopmentBookBasketPrice(developmentBookBasket);
-		assertEquals(finalPrice, expectedPrice);
+		assertEquals(finalPrice, expectedPrice);*/
 	}
 
 
