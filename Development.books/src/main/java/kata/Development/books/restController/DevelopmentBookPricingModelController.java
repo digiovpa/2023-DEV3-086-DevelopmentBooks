5c2a4618ed5791f69a1d4e2a0e6d703749243ec5
digiovpa <paolodigiov@gmail.com>
2023-09-08 12:22:00 +0200

    - Started the implementation of the rest controller - Setup a server port on the application.properties

--- Development.books/src/main/java/kata/Development/books/restController/DevelopmentBookPricingModelController.java
+++ Development.books/src/main/java/kata/Development/books/restController/DevelopmentBookPricingModelController.java
@@ -1,18 +1,19 @@
 package kata.Development.books.restController;
 
-import kata.Development.books.entity.DevelopmentBook;
 import kata.Development.books.service.DevelopmentBookPricingServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.RoundingMode;
 import java.util.*;
 
 @RestController
 @RequestMapping(path = "/development-books")
 public class DevelopmentBookPricingModelController {
+    @GetMapping("/compute-pricing")
+    private BigDecimal getDevelopmentBooksPricing (@RequestParam Map<String, String> developmentBookBasketMap ) {
+        DevelopmentBookPricingServiceImpl developmentBookPricingService = new DevelopmentBookPricingServiceImpl();
+        final Map<Integer, Integer> convertedMap = new HashMap<>();
+        return developmentBookPricingService.computeDevelopmentBookBasketPrice(convertedMap);
+    }
 
 }
